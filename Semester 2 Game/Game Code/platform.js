84b5b14b168d0b6a161da5f291efb5ec029992dd
SethReis <795091@seq.org>
2020-02-13 13:43:39 -0800

    plat images and other stuff

--- Semester 2 Game/Game Code/platform.js
+++ Semester 2 Game/Game Code/platform.js
@@ -6,16 +6,18 @@ class Platform{
     this.w = w;
     this.h = h;
     this.type = type;
+    this.platImage;
   }
 
   update(){
-
+    this.platImage = game.platformOverlay.get(this.x, this.y, this.w, this.h);
   }
 
   render(){
     noStroke();
     fill(255);
-    rect(this.x, this.y, this.w, this.h);
+    //rect(this.x, this.y, this.w, this.h);
+    image(this.platImage, this.x, this.y);
     stroke(0, 0, 0);
     fill(255, 0, 0);
   }
