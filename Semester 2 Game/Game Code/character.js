79bc92dd7374ff450b3b3ad513a4de5818ef7a37
SethReis <795091@seq.org>
2020-03-09 13:45:35 -0700

    rg

--- Semester 2 Game/Game Code/character.js
+++ Semester 2 Game/Game Code/character.js
@@ -84,7 +84,7 @@ class Character{
         sideState = 3;
       }
       //seperate, check if on platform
-      if(this.loc.x+20 > plat.x && this.loc.x-20 < plat.x + plat.w && this.loc.y+1 > plat.y && this.loc.y+1 < plat.y + plat.h){
+      if(this.loc.x+20 > plat.x && this.loc.x-20 < plat.x + plat.w && this.loc.y+1 > plat.y && this.loc.y-40 < plat.y + plat.h){
         this.onPlatform = true;
       }
       if(sideState === 1){
